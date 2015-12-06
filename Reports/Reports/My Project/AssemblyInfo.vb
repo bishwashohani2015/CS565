65b6daa0e5913686eea6e56132ee35f246d08688
Bishwash <itsmebiss2015@gmail.com>
2015-12-05 18:42:05 -0600

    Crystal Reports
    Crystal Reports

---
+++ Reports/Reports/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Reports")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Reports")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("5ef287c8-2d1a-4b31-b61e-7d9608de5052")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
